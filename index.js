8e09a018aef561329f7a5b8393458917192f16cb
Max <unitigrand@gmail.com>
2021-09-26 11:54:57 +0300

    MySQL todo second step

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const path = require('path');
+const sequelize = require('./utils/database');
 const todoRoutes = require('./routes/todo');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
 app.use('/app/todo', todoRoutes);
 app.use((req, res, next) => {
     res.sendFile('/index.html');
 });
 
-app.listen(PORT);
+async function start() {
+    try {
+        await sequelize.sync();
+        app.listen(PORT);
+    } catch(e) {
+        console.log(e);
+    }
+}
+
+start();
--- public/app.js
+++ public/app.js
@@ -8,34 +8,74 @@ new Vue({
         todos: []
       }
     },
+    created() {
+        fetch('/app/todo', {
+            method: 'get'
+        }).then(res => res.json())
+        .then(todos => {
+           this.todos = todos;
+        })
+        .catch(e => console.log(e));
+    },
     methods: {
       addTodo() {
         const title = this.todoTitle.trim()
         if (!title) {
-          return
+          return;
         }
-        this.todos.push({
-          title: title,
-          id: Math.random(),
-          done: false,
-          date: new Date()
-        })
-        this.todoTitle = ''
+       fetch('/api/todo', {
+           method: 'post',
+           headers: {'Content type': 'aplication/json'},
+           body: JSON.stringify({title})
+       })
+       .then(res => res.json())
+       .then(({todo}) =>{
+           
+        this.todos.push(todo);
+         this.todoTitle = '';
+       })
+       .catch(e =>console.log(e));
       },
       removeTodo(id) {
-        this.todos = this.todos.filter(t => t.id !== id)
-      }
+          fetch('/api/todo' + id, {
+              method: 'delete'
+          })
+          .then(() =>{
+            this.todos = this.todos.filter(t => t.id !== id)
+          }).catch( e => console.log(e));
+        }
+    },
+    completeTodo(id) {
+        fetch('/api/todo/' + id, {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({done: true})
+        })
+        .then(res => res.json())
+        .then(({todo})=> {
+            const idx = this.todos.findIndex(t => t.id === todo.id);
+            this.todos[idx].updateAt = todo.updateAt;
+        })
     },
     filters: {
       capitalize(value) {
         return value.toString().charAt(0).toUpperCase() + value.slice(1)
       },
-      date(value) {
-        return new Intl.DateTimeFormat('ru-RU', {
-          year: 'numeric',
-          month: 'long',
-          day: '2-digit'
-        }).format(new Date(value))
+      date(value, withTime) {
+          const options = {
+            year: 'numeric',
+            month: 'long',
+            day: '2-digit'
+          };
+          if(withTime) {
+              options.hour = '2-digit';
+              options.minute = '2-digit';
+              options.second = '2-digit';
+
+          }
+       
+        return new Intl.DateTimeFormat('ru-RU', options)
+        .format(new Date(value));
       }
     }
-  })
+  });
